020a0255a810e72c13fde2ba69e8bd3941a302b2
Brendan Kennedy <brenwken@gmail.com>
2023-08-03 06:55:28 +0000

    Add swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app.module'
+import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(4000);
+  const app = await NestFactory.create(AppModule, { cors: true })
+
+  const config = new DocumentBuilder()
+    .setTitle('Big Boat Move Stuff API')
+    .setDescription('The bbms API description')
+    .setVersion('1.0')
+    .addTag('bbms')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
+  await app.listen(4000)
 }
-bootstrap();
+
+bootstrap()
